186af61be4cf77460ac53c9f90178011333dc9e9
최다영 <rtyou@naver.com>
2022-03-05 14:46:04 +0900

    뷰페이저 어댑터의 실제 기능 구현 3장짜리에서 뭐뭐머 페이지 있는지

--- app/src/main/java/com/example/viewpage/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/example/viewpage/adapters/MainViewPagerAdapter.kt
@@ -1,7 +1,34 @@
 package com.example.viewpage.adapters
 
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.example.viewpage.fragments.HelloFragment
+import com.example.viewpage.fragments.MyInfoFragments
+import com.example.viewpage.fragments.MyNameFragments
 
 class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount(): Int {
+        TODO("Not yet implemented")
+
+        return 3
+
+    }
+
+    override fun getItem(position: Int): Fragment {
+        TODO("Not yet implemented")
+
+        if(position == 0) {
+            return MyNameFragments()
+        }
+
+        else if (position ==1) {
+            return MyInfoFragments()
+
+        }
+        else {
+            return HelloFragment()
+
+        }
+    }
 }
